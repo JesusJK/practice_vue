19eb5aa943cedc374a6dd8d59101d75e745cb7b4
JesusJK <camilojesus19@gmail.com>
2024-06-23 10:20:22 -0500

    solucion de pares e impar

--- src/vue/Pares_impar.vue
+++ src/vue/Pares_impar.vue
@@ -1,27 +1,32 @@
 <script setup>
 import { ref } from 'vue';
 
+const setnumber = ref([]);
+
 const numbers = ref([]);
-const setnumber = ref('');
+const number_par = ref([]);
+const number_impar = ref([]);
 
 function sendn() {
     numbers.value.push(setnumber.value)
-    console.log(numbers)
+
 }
 
+function esPar(num) {
+    return num % 2 === 0;
+}
+
+function esImpar(num) {
+    return num % 2 !== 0;
+}  
+
 function n_par() {
-    for (let i = 0; i < numbers.length; i++) {
-        if (numbers[i] % 2 == 0) {
-            const n_pares = ([numbers[i]])
-        }
-        
-    }
+    number_par.value = numbers.value.filter(esPar);         //filter para filtrar segun una función
 }
 
 function n_impar() {
-    
+    number_impar.value = numbers.value.filter(esImpar);
 }
-
 
 </script>
 
@@ -29,8 +34,10 @@ function n_impar() {
     <div class="bg-slate-300 shadow rounded mx-auto mt-1 px-8 py-10">
 
         <div id="introducir" class="m-2">
-            <button @click="sendn()" class="bg-gray-500 text-white p-2 rounded mx-1">Enviar</button>
             <input v-model="setnumber" placeholder="Introducir un numero" type="number" class="font-semibold m-1 p-1">
+
+            <button @click="sendn()" class="bg-gray-500 text-white p-2 rounded mx-1">Enviar</button>
+
             <button @click="n_par()" class="text-white border rounded p-1 m-2 w-[50px] bg-green-500">Par</button>
             <button @click="n_impar()" class="text-white border rounded p-1 m-2 bg-red-500">Impar</button>
         </div>
@@ -38,16 +45,18 @@ function n_impar() {
 
         <div id="pantalla" class="bg-white flex justify-between">
             <div id="par" class="border-gray-600 border mx-1 w-[50%]">
-                <Strong class="flex justify-center">Numeros Pares</Strong>
-                        <div v-for="(num, index) in numbers" :key="index" class="bg-white/60 px-2">{{ numbers }}</div>
-                  
+                <strong class="flex justify-center">Numeros Pares</strong>
+                <div v-for="(number, index) in number_par" :key="index" class="bg-white/60 px-2">
+                    {{ number }}
+                </div>
+
             </div>
 
             <div id="impar" class="border-gray-600 border mx-1 w-[50%]">
                 <strong class="flex justify-center">Numeros Impares</strong>
-                <ul>
-                    <li></li>
-                </ul>
+                <div v-for="(number, index) in number_impar" :key="index" class="bg-white/60 px-2">
+                    {{ number }}
+                </div>
             </div>
 
         </div>
